350a4b807358c75e6ef6565eaf736ba7364fca3a
Timofey Kazantsev <pianoist@ya.ru>
2020-02-20 07:03:34 +0700

    fix startstring

--- session_management/tests/slave.py
+++ session_management/tests/slave.py
@@ -2,6 +2,6 @@ from session_management.connections import jack_backend as jb
 
 back = jb.JackBackend(
     '192.168.2.2',
-    'jackd -R -S -d net -a 192.168.2.2 -p 9002 -n "192.168.2.2"',
+    'jackd -R -S -d net -a 192.168.2.1 -p 9002 -i 10 -o 0 -C 1 -P 2 -n 192.168.2.2  >/dev/null 2>&1 < /dev/null &',
     is_master=False
 )
